ce65dd5d02ffae976f291b318515fba0430fc8e1
Erik Baliov <vfxbaliof@gmail.com>
2018-03-26 15:14:54 +0300

    Implement Extra Functionality - Input Validation

--- JAVASCRIPT EXPRESSJS OVERVIEW/controllers/home.js
+++ JAVASCRIPT EXPRESSJS OVERVIEW/controllers/home.js
@@ -1,7 +1,7 @@
 const Calculator = require('../models/Calculator');
 module.exports = {
     indexGet: (req, res) => {
-        res.render('home/index');
+        res.render('home/index', {'leftOperandPlaceholder':'Left operand', 'rightOperandPlaceholder':'Right operand', 'resultPlaceholder':'Result', 'leftOperandColor':'#ffffff', 'rightOperandColor':'#ffffff'});
     },
 
     indexPost: (req, res) => {
@@ -9,12 +9,23 @@ module.exports = {
 
         let calculatorParams = calculatorBody['calculator'];
         let calculator = new Calculator();
-        calculator.leftOperand = Number(calculatorParams.leftOperand);
-        calculator.operator = calculatorParams.operator;
-        calculator.rightOperand = Number(calculatorParams.rightOperand);
 
-        let result = calculator.calculateResult();
+        if(calculatorParams.leftOperand != '' && calculatorParams.rightOperand != '') {
 
-        res.render('home/index', {'calculator': calculator, 'result':result});
+            calculator.leftOperand = Number(calculatorParams.leftOperand);
+            calculator.operator = calculatorParams.operator;
+            calculator.rightOperand = Number(calculatorParams.rightOperand);
+            let result = calculator.calculateResult();
+
+            res.render('home/index', {'calculator': calculator, 'result': result, 'leftOperandColor':'#ffffff', 'rightOperandColor':'#ffffff'});
+        }
+        else if(calculatorParams.leftOperand == ''){
+            calculator.rightOperand = Number(calculatorParams.rightOperand);
+            res.render('home/index', {'calculator': calculator,'leftOperandPlaceholder':'Enter a number', 'resultPlaceholder':'Result', 'leftOperandColor':'#ff0000', 'rightOperandColor':'#ffffff'});
+        }
+        else if(calculatorParams.rightOperand == ''){
+            calculator.leftOperand = Number(calculatorParams.leftOperand);
+            res.render('home/index', {'calculator': calculator,'rightOperandPlaceholder':'Enter a number', 'resultPlaceholder':'Result', 'rightOperandColor':'#ff0000', 'leftOperandColor':'#ffffff'});
+        }
     }
 };
