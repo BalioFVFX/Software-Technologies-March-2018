664ddfe039321302351829d2e24204117dfc7ad2
Erik Baliov <vfxbaliof@gmail.com>
2018-03-29 15:53:23 +0300

    04. Product of 3 Numbers

---
+++ PHP SYNTAX AND BASIC WEB - EXERCISES/04. Product of 3 Numbers/04. Product of 3 Numbers.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First Steps Into PHP</title>
+
+</head>
+<body>
+    <form>
+        X: <input type="text" name="num1" />
+		Y: <input type="text" name="num2" />
+        Z: <input type="text" name="num3" />
+		<input type="submit" />
+    </form>
+    <?php
+    if(isset($_GET['num1']) && isset($_GET['num2']) && isset($_GET['num3'])){
+        $numbers = array(intval($_GET['num1']), intval($_GET['num2']), intval($_GET['num3']));
+        $negativeCount = 0;
+        foreach($numbers as $number){
+            if($number < 0){
+                $negativeCount++;
+            }
+            else if($number == 0){
+                echo "Positive";
+                return;
+            }
+        }
+        if($negativeCount % 2 == 0){
+            echo "Positive";
+        }
+        else{
+            echo "Negative";
+        }
+    }
+    ?>
+</body>
+</html>
