ce26f8d0039bbd3caed55cb1e2d97c7da620d14d
Erik Baliov <vfxbaliof@gmail.com>
2018-04-05 12:28:27 +0300

    01. Variable in Hex Format

---
+++ JAVA BASIC SYNTAX - EXERCISES/src/VariableInHexFormat/VariableInHexFormat.java
@@ -0,0 +1,10 @@
+package VariableInHexFormat;
+
+import java.util.Scanner;
+
+public class VariableInHexFormat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Integer.parseInt(scanner.nextLine(), 16));
+    }
+}
