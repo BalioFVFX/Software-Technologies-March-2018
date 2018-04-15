3f3409c2545a5891987778f3d02592b0e4db9e42
Erik Baliov <vfxbaliof@gmail.com>
2018-04-15 15:35:07 +0300

    01. Numbers from 1-50

---
+++ C# ASP.NET MVC OVERVIEW/01. Numbers from 1-50/0.1 Numbers from 1-50/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace _0._1_Numbers_from_1_50.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
