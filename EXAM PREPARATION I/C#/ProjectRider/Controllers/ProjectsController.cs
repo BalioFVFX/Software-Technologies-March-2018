cc64c7ad3b1ff93aab3f50f00f2d1ab400a8c6df
Erik Baliov <vfxbaliof@gmail.com>
2018-04-19 15:56:48 +0300

    C#

---
+++ EXAM PREPARATION I/C#/ProjectRider/Controllers/ProjectsController.cs
@@ -0,0 +1,89 @@
+﻿namespace ProjectRider.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ProjectRider.Models;
+    using System.Linq;
+
+    public class ProjectController : Controller
+    {
+        private readonly ProjectDbContext context;
+
+        public ProjectController(ProjectDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            // Get all projects from the database
+
+            var projects = context.Projects.ToList();
+
+            return View(projects);
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public ActionResult Create(Project project)
+        {
+            // Save the project in the database and redirect to index
+
+            context.Add(project);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            // Find the project in the database by id
+            var project = context.Projects.Find(id);
+
+            return View(project);
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int id, Project projectModel)
+        {
+            // Update and save the database with the new project from the form
+            context.Projects.Update(projectModel);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            // Find the project in the database by id
+            var project = context.Projects.Find(id);
+
+            return View(project);
+        }
+
+        [HttpPost]
+        [Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int id, Project projectModel)
+        {
+            context.Projects.Remove(projectModel);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
