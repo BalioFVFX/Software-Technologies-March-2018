9bbae6a9bc0c81065d20db99ae74bb73346c8eee
Erik Baliov <vfxbaliof@gmail.com>
2018-04-21 11:12:16 +0300

    PHP

---
+++ EXAM PREPARATION II/TeisterMask/PHP-Skeleton/src/AppBundle/Controller/TaskController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Task;
+use AppBundle\Form\TaskType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * @param Request $request
+     * @Route("/", name="index")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(Request $request)
+    {
+        // Get all tasks by their status in the database
+        $openTasks = $this->getDoctrine()->getRepository(Task::class)->findBy(array("status" => "Open"));
+        $inProgressTasks = $this->getDoctrine()->getRepository(Task::class)->findBy(array("status" => "In Progress"));
+        $finishedTasks = $this->getDoctrine()->getRepository(Task::class)->findBy(array("status" => "Finished"));
+
+        return $this->render("task/index.html.twig", ["openTasks" => $openTasks, "inProgressTasks" => $inProgressTasks, "finishedTasks" => $finishedTasks]);
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/create", name="create")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function create(Request $request)
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+            return $this->redirectToRoute("index");
+        }
+
+        return $this->render("task/create.html.twig", ["form" => $form->createView()]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     *
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function edit($id, Request $request)
+    {
+        $task = $this->getDoctrine()->getRepository(Task::class)->find($id);
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+
+            return $this->redirectToRoute("index");
+        }
+
+        return $this->render("task/edit.html.twig", ["form" => $form->createView(), "task" => $task]);
+    }
+}
